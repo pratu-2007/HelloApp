e69bbd0cc19aceef28cf18fd88c1d33a1bf01228
pratu-2007 <pu5949@srmist.edu.in>
2026-02-22 21:46:40 +0530

    Restore OOPSBannerApp.java source file

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * UC1: Print OOPS to console
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
